2572dd2d7535c92ba6ad19d25bea65803faee93a
FelixMittermeier <kontakt@felix-mittermeier.de>
2021-03-13 20:12:24 +0100

    Simplified the page and improved the done button switch

--- lib/src/introduction_screen.dart
+++ lib/src/introduction_screen.dart
@@ -10,98 +10,19 @@ import 'package:introduction_screen/src/ui/intro_button.dart';
 import 'package:introduction_screen/src/ui/intro_page.dart';
 
 class IntroductionScreen extends StatefulWidget {
-  /// All pages of the onboarding
   final List<PageViewModel> pages;
-
-  /// Callback when Done button is pressed
   final VoidCallback onDone;
-
-  /// Done button
   final Widget done;
-
-  /// Callback when Skip button is pressed
-  final VoidCallback? onSkip;
-
-  /// Callback when page change
   final ValueChanged<int>? onChange;
-
-  /// Skip button
-  final Widget? skip;
-
-  /// Next button
-  final Widget? next;
-
-  /// Is the Skip button should be display
-  ///
-  /// @Default `false`
-  final bool showSkipButton;
-
-  /// Is the Next button should be display
-  ///
-  /// @Default `true`
-  final bool showNextButton;
-
-  /// Is the progress indicator should be display
-  ///
-  /// @Default `true`
   final bool isProgress;
-
-  /// Enable or not onTap feature on progress indicator
-  ///
-  /// @Default `true`
   final bool isProgressTap;
-
-  /// Is the user is allow to change page
-  ///
-  /// @Default `false`
   final bool freeze;
-
-  /// Global background color (only visible when a page has a transparent background color)
   final Color? globalBackgroundColor;
-
-  /// Dots decorator to custom dots color, size and spacing
   final DotsDecorator dotsDecorator;
-
-  /// Animation duration in millisecondes
-  ///
-  /// @Default `350`
   final int animationDuration;
-
-  /// Index of the initial page
-  ///
-  /// @Default `0`
   final int initialPage;
-
-  /// Flex ratio of the skip button
-  ///
-  /// @Default `1`
-  final int skipFlex;
-
-  /// Flex ratio of the progress indicator
-  ///
-  /// @Default `1`
-  final int dotsFlex;
-
-  /// Flex ratio of the next/done button
-  ///
-  /// @Default `1`
-  final int nextFlex;
-
-  /// Type of animation between pages
-  ///
-  /// @Default `Curves.easeIn`
   final Curve curve;
-
-  /// Color of buttons
   final Color? color;
-
-  /// Color of skip button
-  final Color? skipColor;
-
-  /// Color of next button
-  final Color? nextColor;
-
-  /// Color of done button
   final Color? doneColor;
 
   const IntroductionScreen({
@@ -109,12 +30,7 @@ class IntroductionScreen extends StatefulWidget {
     required this.pages,
     required this.onDone,
     required this.done,
-    this.onSkip,
     this.onChange,
-    this.skip,
-    this.next,
-    this.showSkipButton = false,
-    this.showNextButton = true,
     this.isProgress = true,
     this.isProgressTap = true,
     this.freeze = false,
@@ -122,21 +38,13 @@ class IntroductionScreen extends StatefulWidget {
     this.dotsDecorator = const DotsDecorator(),
     this.animationDuration = 350,
     this.initialPage = 0,
-    this.skipFlex = 1,
-    this.dotsFlex = 1,
-    this.nextFlex = 1,
     this.curve = Curves.easeIn,
     this.color,
-    this.skipColor,
-    this.nextColor,
     this.doneColor,
   })  : assert(
           pages.length > 0,
           "You provide at least one page on introduction screen !",
         ),
-        assert((showSkipButton && skip != null) || !showSkipButton),
-        assert((showNextButton && next != null) || !showNextButton),
-        assert(skipFlex >= 0 && dotsFlex >= 0 && nextFlex >= 0),
         assert(initialPage >= 0),
         super(key: key);
 
@@ -147,8 +55,6 @@ class IntroductionScreen extends StatefulWidget {
 class IntroductionScreenState extends State<IntroductionScreen> {
   late PageController _pageController;
   double _currentPage = 0.0;
-  bool _isSkipPressed = false;
-  bool _isScrolling = false;
 
   PageController get controller => _pageController;
 
@@ -164,32 +70,12 @@ class IntroductionScreenState extends State<IntroductionScreen> {
     animateScroll(min(_currentPage.round() + 1, widget.pages.length - 1));
   }
 
-  Future<void> _onSkip() async {
-    if (widget.onSkip != null) {
-      widget.onSkip!();
-    } else {
-      await skipToEnd();
-    }
-  }
-
-  Future<void> skipToEnd() async {
-    setState(() => _isSkipPressed = true);
-    await animateScroll(widget.pages.length - 1);
-    if (mounted) {
-      setState(() => _isSkipPressed = false);
-    }
-  }
-
   Future<void> animateScroll(int page) async {
-    setState(() => _isScrolling = true);
     await _pageController.animateToPage(
       page,
       duration: Duration(milliseconds: widget.animationDuration),
       curve: widget.curve,
     );
-    if (mounted) {
-      setState(() => _isScrolling = false);
-    }
   }
 
   bool _onScroll(ScrollNotification notification) {
@@ -203,19 +89,6 @@ class IntroductionScreenState extends State<IntroductionScreen> {
   @override
   Widget build(BuildContext context) {
     final isLastPage = (_currentPage.round() == widget.pages.length - 1);
-    bool isSkipBtn = (!_isSkipPressed && !isLastPage && widget.showSkipButton);
-
-    final skipBtn = IntroButton(
-      child: widget.skip ?? Container(),
-      color: widget.skipColor ?? widget.color,
-      onPressed: isSkipBtn ? _onSkip : null,
-    );
-
-    final nextBtn = IntroButton(
-      child: widget.next ?? Container(),
-      color: widget.nextColor ?? widget.color,
-      onPressed: widget.showNextButton && !_isScrolling ? next : null,
-    );
 
     final doneBtn = IntroButton(
       child: widget.done,
@@ -231,50 +104,35 @@ class IntroductionScreenState extends State<IntroductionScreen> {
             onNotification: _onScroll,
             child: PageView(
               controller: _pageController,
-              physics: widget.freeze
-                  ? const NeverScrollableScrollPhysics()
-                  : const BouncingScrollPhysics(),
+              physics: widget.freeze ? const NeverScrollableScrollPhysics() : const BouncingScrollPhysics(),
               children: widget.pages.map((p) => IntroPage(page: p)).toList(),
               onPageChanged: widget.onChange,
             ),
           ),
           Positioned(
             bottom: 16.0,
-            left: 16.0,
-            right: 16.0,
+            left: 0,
+            right: 0,
             child: SafeArea(
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: widget.skipFlex,
-                    child: isSkipBtn
-                        ? skipBtn
-                        : Opacity(opacity: 0.0, child: skipBtn),
-                  ),
-                  Expanded(
-                    flex: widget.dotsFlex,
-                    child: Center(
-                      child: widget.isProgress
-                          ? DotsIndicator(
-                              dotsCount: widget.pages.length,
-                              position: _currentPage,
-                              decorator: widget.dotsDecorator,
-                              onTap: widget.isProgressTap && !widget.freeze
-                                  ? (pos) => animateScroll(pos.toInt())
-                                  : null,
-                            )
-                          : const SizedBox(),
-                    ),
-                  ),
-                  Expanded(
-                    flex: widget.nextFlex,
-                    child: isLastPage
-                        ? doneBtn
-                        : widget.showNextButton
-                            ? nextBtn
-                            : Opacity(opacity: 0.0, child: nextBtn),
-                  ),
-                ],
+              child: Container(
+                constraints: const BoxConstraints(minHeight: 54.0),
+                child: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 150),
+                  switchInCurve: Curves.easeInOut,
+                  switchOutCurve: Curves.easeInOut,
+                  child: isLastPage
+                      ? doneBtn
+                      : Center(
+                          child: widget.isProgress
+                              ? DotsIndicator(
+                                  dotsCount: widget.pages.length,
+                                  position: _currentPage,
+                                  decorator: widget.dotsDecorator,
+                                  onTap: widget.isProgressTap && !widget.freeze ? (pos) => animateScroll(pos.toInt()) : null,
+                                )
+                              : const SizedBox(),
+                        ),
+                ),
               ),
             ),
           ),
